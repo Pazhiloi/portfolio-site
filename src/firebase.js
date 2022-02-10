b0e7f91fef9e377c7dc0fd6ee14986bea9f48c94
Pazhiloi <martinsen06@gmail.com>
2022-02-10 16:32:39 +0200

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNdjEZV1Pz53jAT8JCAa1zv6dSg9IvVb8",
+  authDomain: "portfolio-a2d54.firebaseapp.com",
+  projectId: "portfolio-a2d54",
+  storageBucket: "portfolio-a2d54.appspot.com",
+  messagingSenderId: "297469380546",
+  appId: "1:297469380546:web:8aa0e0a9893f6b5973385b",
+};
